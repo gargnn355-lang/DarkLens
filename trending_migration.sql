294e7e19f7bd1b8b5ab3b162f02554b554ef23ea
gargnn355-lang <gargnn355@gmail.com>
2025-11-12 21:07:03 +0530

    updated code

--- trending_migration.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE onion_links ADD COLUMN last_crawled_at timestamptz, ADD COLUMN source_urls text[], ADD COLUMN source_count integer DEFAULT 1, ADD COLUMN trending_score numeric DEFAULT 0;
